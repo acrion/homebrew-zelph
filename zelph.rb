1aba35cbed300a8cfa6992ee05cb59d5860e46c0
Stefan Zipproth <s.zipproth@acrion.ch>
2026-01-05 18:51:08 +0100

    v0.9.2

---
+++ zelph.rb
@@ -0,0 +1,18 @@
+class Zelph < Formula
+    desc "A sophisticated semantic network system for logical reasoning"
+    homepage "https://zelph.org"
+    url "https://github.com/acrion/zelph/releases/download/v0.9.2/zelph-macos.zip"
+    sha256 "85199f378a14e5e6b4d8bec52bac09a13ac428a55d93cb41e4c8412b3e811953"
+    license "AGPL-3.0-or-later"
+  
+    def install
+      libexec.install Dir["*"]
+      bin.install_symlink libexec/"zelph"
+    end
+  
+    test do
+      output = pipe_output("#{bin}/zelph", ".exit\n")
+      assert_match version.to_s, output
+    end
+  end
+  
