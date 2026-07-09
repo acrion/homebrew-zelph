bfd05339e2e55647807e25d54d6d25a4979b7d68
Stefan Zipproth <s.zipproth@acrion.ch>
2026-07-09 23:41:37 +0200

    v0.9.8

--- zelph.rb
+++ zelph.rb
@@ -1,8 +1,8 @@
 class Zelph < Formula
     desc "A sophisticated semantic network system for logical reasoning"
     homepage "https://zelph.org"
-    url "https://github.com/acrion/zelph/releases/download/v0.9.7/zelph-macos.zip"
-    sha256 "b62b82ba7bd630afa09fd45ba13eb6e28b06118e54a9a653e84da027a7852c7d"
+    url "https://github.com/acrion/zelph/releases/download/v0.9.8/zelph-macos.zip"
+    sha256 "90e939633b6acf4a93364bac40e704c9d0bf424566d7dd58327897e20b2e7abd"
     license "AGPL-3.0-or-later"
 
     def install
